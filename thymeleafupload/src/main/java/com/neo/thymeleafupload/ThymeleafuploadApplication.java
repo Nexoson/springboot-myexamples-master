d2538d377192a70339a9fcee86ee5b7b554129f0
Nexoson <1018458278@qq.com>
2019-10-28 21:17:13 +0800

    文件上传,初次上传

---
+++ thymeleafupload/src/main/java/com/neo/thymeleafupload/ThymeleafuploadApplication.java
@@ -0,0 +1,13 @@
+package com.neo.thymeleafupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafuploadApplication.class, args);
+    }
+
+}
---
+++ thymeleafupload/src/test/java/com/neo/thymeleafupload/ThymeleafuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.thymeleafupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafuploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
