700f86b0c0a67f1efaed3f03a889d6bb988d9ef1
Nexoson <1018458278@qq.com>
2020-05-06 22:34:23 +0800

    增加打包配置

--- spring-boot-websocket/src/main/java/com/neo/websocket/SpringBootWebsocketApplication.java
+++ spring-boot-websocket/src/main/java/com/neo/websocket/SpringBootWebsocketApplication.java
@@ -2,22 +2,29 @@ package com.neo.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @EnableWebSocket
 @SpringBootApplication
-public class SpringBootWebsocketApplication {
+public class SpringBootWebsocketApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebsocketApplication.class, args);
     }
 
     @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
+    public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
 
 
+    // 为打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
