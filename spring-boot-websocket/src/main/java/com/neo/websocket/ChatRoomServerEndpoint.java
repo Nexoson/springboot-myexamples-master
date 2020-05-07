b4372e4700716054741032fee634ce049af9a80f
Nexoson <1018458278@qq.com>
2020-05-07 16:24:21 +0800

    增加自动回复

--- spring-boot-websocket/src/main/java/com/neo/websocket/ChatRoomServerEndpoint.java
+++ spring-boot-websocket/src/main/java/com/neo/websocket/ChatRoomServerEndpoint.java
@@ -8,9 +8,11 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 
-import static com.neo.websocket.utils.WebSocketUtils.ONLINE_USER_SESSIONS;
-import static com.neo.websocket.utils.WebSocketUtils.sendMessageAll;
+import static com.neo.websocket.utils.WebSocketUtils.*;
 
 
 /**
@@ -25,31 +27,44 @@ import static com.neo.websocket.utils.WebSocketUtils.sendMessageAll;
 @ServerEndpoint("/chat-room/{username}")
 public class ChatRoomServerEndpoint {
     private static final Logger logger = LoggerFactory.getLogger(ChatRoomServerEndpoint.class);
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @OnOpen
-    public void openSession(@PathParam("username") String username, Session session){
-        ONLINE_USER_SESSIONS.put(username,session);
+    public void openSession(@PathParam("username") String username, Session session) {
+        ONLINE_USER_SESSIONS.put(username, session);
         String message = "欢迎用户[" + username + "] 来到聊天室！";
-        logger.info("用户登录："+message);
-        sendMessageAll(message);
+        logger.info("用户登录：" + message);
+        // 首次触发机器人信息
+        if (ONLINE_USER_SESSIONS.keySet().size() > 1) {
+            sendMessageAll(LocalDateTime.now().format(formatter) + message);
+        } else {
+            getReportMsg(session, null);
+        }
     }
 
     @OnMessage
-    public void onMessage(@PathParam("username") String username, String message){
-        logger.info("发送消息: "+ message);
-        sendMessageAll("用户[" + username + "] : " + message);
+    public void onMessage(@PathParam("username") String username, String message) {
+        logger.info("发送消息: " + message);
+        if (ONLINE_USER_SESSIONS.keySet().size() > 1) {
+            sendMessageAll(LocalDateTime.now().format(formatter) + " 用户[" + username + "] : " + message);
+        } else {
+            getReportMsg(ONLINE_USER_SESSIONS.get(username), message);
+        }
     }
 
     @OnClose
-    public void onClose (@PathParam("username") String username, Session session) {
+    public void onClose(@PathParam("username") String username, Session session) {
         // 当前的Session 移除
         ONLINE_USER_SESSIONS.remove(username);
         // 并且通知其他人当前用户已经离开聊天室了
-        sendMessageAll("用户[" + username + "] 已经离开聊天室了！");
+        sendMessageAll(LocalDateTime.now().format(formatter) + " 用户[" + username + "] 已经离开聊天室了！");
+        if (ONLINE_USER_SESSIONS.keySet().size() == 1) {
+            ONLINE_USER_SESSIONS.forEach((sessionId, onesession) -> getReportMsg(onesession, null));
+        }
         try {
             session.close();
         } catch (IOException e) {
-            logger.error("onClose error",e);
+            logger.error("onClose error", e);
         }
     }
 
@@ -58,9 +73,9 @@ public class ChatRoomServerEndpoint {
         try {
             session.close();
         } catch (IOException e) {
-            logger.error("onError excepiton",e);
+            logger.error("onError excepiton", e);
         }
-        logger.info("Throwable msg "+throwable.getMessage());
+        logger.info("Throwable msg " + throwable.getMessage());
     }
 
 }
--- spring-boot-websocket/src/main/java/com/neo/websocket/utils/WebSocketUtils.java
+++ spring-boot-websocket/src/main/java/com/neo/websocket/utils/WebSocketUtils.java
@@ -1,11 +1,14 @@
 package com.neo.websocket.utils;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -23,26 +26,70 @@ public final class WebSocketUtils {
     // 存储 websocket session
     public static final Map<String, Session> ONLINE_USER_SESSIONS = new ConcurrentHashMap<>();
 
+    // 存储机器人话术
+    public static final Map<Integer, String> ONLINE_REPORT_WORDS = new ConcurrentHashMap<>();
+
+    // 存储机器人回答
+    public static final Map<Integer, String> ONLINE_REPORT_ANSWERS = new ConcurrentHashMap<>();
+
+    static {
+        ONLINE_REPORT_WORDS.put(0, "你好! 很高兴为你服务,有什么可以帮到您的吗?");
+        ONLINE_REPORT_WORDS.put(1, "\n1.什么时候提车呢?");
+        ONLINE_REPORT_WORDS.put(2, "\n2.提车手续费是指什么?");
+        ONLINE_REPORT_WORDS.put(3, "\n3.上路手续怎么办理?");
+        ONLINE_REPORT_WORDS.put(4, "\n4.都有什么颜色呢?");
+        ONLINE_REPORT_WORDS.put(5, "\n5.可以帮忙办理分期吗?");
+
+        ONLINE_REPORT_ANSWERS.put(1, "\n您好，提车时间因订单火爆目前无法保证，您支付尾款之后会尽快安排哈，咱们这里尽快帮您办理手续！");
+        ONLINE_REPORT_ANSWERS.put(2, "\n车辆购置税,车船使用税,强险,上牌费回");
+        ONLINE_REPORT_ANSWERS.put(3, "\n如果在我们门店未覆盖地区是无法帮忙办理上牌、保险和购置税的，只能提供裸车哦！目前我们门店遍布于云南、贵州、四川、重庆、湖南、广东、广西、江西、福建。其他地区是没有的哈。");
+        ONLINE_REPORT_ANSWERS.put(4, "\n塔夫绸白,海洋蓝,冰河银,天空蓝,绽放橙,鲜韵绿");
+        ONLINE_REPORT_ANSWERS.put(5, "\n我们这有多种金融方案,可以帮忙办理分期的");
+    }
+
     /**
      * @param session 用户 session
      * @param message 发送内容
      */
-    public static void sendMessage(Session session, String message){
-        if(null == session){
-            return ;
+    public static void sendMessage(Session session, String message) {
+        if (null == session) {
+            return;
         }
         final RemoteEndpoint.Basic basic = session.getBasicRemote();
-        if (basic == null){
-            return ;
+        if (basic == null) {
+            return;
         }
-        try{
+        try {
             basic.sendText(message);
-        }catch (IOException e){
-            logger.error("sendMessage IOException ",e);
+        } catch (IOException e) {
+            logger.error("sendMessage IOException ", e);
         }
     }
 
-    public static void sendMessageAll(String message){
+    public static void sendMessageAll(String message) {
         ONLINE_USER_SESSIONS.forEach((sessionId, session) -> sendMessage(session, message));
+    }
+
+
+    /**
+     * @return
+     * @Author huangX
+     * @Date 11:11 2020/5/7
+     * @Param
+     * @Description 触发机器人信息
+     **/
+    public static void getReportMsg(Session session, String msg) {
+
+        // 首次登陆,触发欢迎语(msg 为null)
+        if (null == msg) {
+            sendMessage(session, ONLINE_REPORT_WORDS.values().toString());
+        } else {
+            if (ONLINE_REPORT_ANSWERS.keySet().contains(Integer.valueOf(msg))) {
+                sendMessage(session, ONLINE_REPORT_ANSWERS.get(Integer.valueOf(msg)));
+            } else {
+                sendMessage(session, "请咨询人工客服");
+            }
+
+        }
     }
 }
